a88ec78265e19b86999aff9925437b1894995db1
mohamedhany2024 <hanyrzq06@gmail.com>
2024-12-01 15:57:43 +0200

    finishjs

---
+++ 1.js
@@ -0,0 +1,55 @@
+const resultDiv = document.getElementById("resultDiv");
+
+var equation = "";
+
+const spicalCharacters = ["*", "/"];
+
+
+function addNumber(number) {
+
+    const lastCharacter = equation.slice(-1);
+
+    if (spicalCharacters.includes(lastCharacter) && spicalCharacters.includes(number)) {
+        deleteNumber();        
+    }
+
+    if (equation == "" && specialCharacters.includes(number)) {
+        
+    } else {
+
+        equation = equation + number;
+
+        resultDiv.innerText = equation;
+        
+    }
+}
+
+
+
+function calculate() {
+
+    if (equation != "") {
+        const result = eval(equation);
+        resultDiv.innerText = result;
+        equation = "";
+        
+    }
+    
+}
+
+function clearScreen() {
+    equation = "";
+    resultDiv.innerText = "0";
+    
+}
+
+function deleteNumber() {
+    equation = equation.slice(0, -1);
+    if (equation == "") {
+        resultDiv.innerText = "0";
+        
+    } else {
+        resultDiv.innerText = equation;
+    }
+    
+}
